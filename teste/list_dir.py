219375d2cdd39cac12b3ecbe2f93847b7a51fb68
zellio22 <julienmarg@gmail.com>
2024-10-19 16:07:00 +0200

    beta

--- teste/list_dir.py
+++
@@ -1,9 +0,0 @@
-import os
-def path_check_geng(chemin):
-
-    if not os.path.exists(chemin):
-   
-        os.makedirs(chemin)
-        print(f"Le dossier {chemin} a été créé.")
-    else:
-        print(f"Le dossier {chemin} existe déjà.")
--- teste/pyinstaller.py
+++
@@ -1,2 +0,0 @@
-import sysconfig; print(sysconfig.get_path("scripts"))
-import site; print(site.USER_BASE + "\\Scripts")
